c234cefded948eeaa9e99752a2e2d02c07aab630
Frederic Ernst Neitzel <frederic.e.neitzel@campus.tu-berlin.de>
2019-12-11 12:59:07 +0100

    updated script

--- testing/peer.py
+++ testing/peer.py
@@ -21,82 +21,77 @@ class Logger:
 		self.logger = logging.getLogger(name)
 		self.logger.setLevel(level)
 		self.logger.addHandler(handler)
-		self.logger.info("Cleaning dust from Memory ... and injecting Remote Code")
 
-	def writeToFile(self, line):
-		if self.Logger.propagate == True:
-			self.Logger.debug(line)
+
+	def info(self, line):
+		if self.logger.propagate == True:
+			self.logger.info("Cleaning dust from Memory ... and injecting Remote Code")
 
 	def closeFile(self):
-		if self.Logger.propagate == True:
-			self.Logger.propagate = False
+		if self.logger.propagate == True:
+			self.logger.propagate = False
 
 
 class Tests(unittest.TestCase):
-	def start_peers(self):
-		"""
-				stdout_logger = logging.getLogger('STDOUT')
-		sl = StreamToLogger(stdout_logger, logging.INFO)
-		sys.stdout = sl
-
-		stderr_logger =
-		sl =
-		sys.stderr = sl
-		"""
-		#print "Test to standard out"
-		#raise Exception('Test to standard error')
+	def test_start_peers(self):
 
 		terminal_invocation = 'gnome-terminal -x'
 		abs_build_folder = 'cmake-build-debug'
 		ip = '127.0.0.1'
 
-		peercount = 50
+		self.peercount = 10
 		startport = 4000
-		portrange = np.linspace(startport,startport+peercount-1,peercount)
+		self.portrange = np.linspace(startport,startport+self.peercount-1,self.peercount)
 
 		cmd_lst = [['../'+abs_build_folder+'/peer',ip,str(startport)]]
 
 		idstart = 1
 		idend = 65000
-		idrange = np.linspace(idstart,idend,peercount)
+		idrange = np.linspace(idstart,idend,self.peercount)
 
 		peer_lst = [[ip,startport]]
 
 		def thread_function(cmd,threadnumber,out_logger,err_logger):
 
-			out_logger.info("Starting new Peer "+ threadnumber)
+			#out_logger.info("Starting new Peer "+ threadnumber)
 			process = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True)
 				#print(process.communicate())
 			while True:
 				output = process.stderr.readline()
 				std_out = process.stdout.readline()
 				if (output == b'' or output == '') and process.poll() != None:
-					out_logger.info("\nThread "+threadnumber+" is about to terminate")
-					err_logger.info("\nThread "+threadnumber+" is about to terminate")
+					out_logger.debug("\nThread "+threadnumber+" is about to terminate")
+					err_logger.debug("\nThread "+threadnumber+" is about to terminate")
+					print("Exception with "+ cmd[1] +" "+cmd[2])
+					assert(False)
 					break
 				if output:
-					err_logger.info(output)
-
+					print("ERROR")
+					err_logger.debug(output)
 				if std_out:
 					out_logger.info(std_out)
+					print(" WE ARE IN STDOUT")
 
 			rc = process.poll()
 			err_logger.info("Terminated")
-			out_logger.info("Terminated")
+			#out_logger.info("Terminated")
 
-		for i in range(1,peercount):
+		for i in range(1,self.peercount):
 			dst_peer = random.choice(peer_lst)
-			cmd_lst.append(['../'+abs_build_folder+'/peer',str(ip),str(int(portrange[i])),str(int(idrange[i])),str(dst_peer[0]),str(dst_peer[1])])
-			peer_lst.append([str(ip),str(int(portrange[i]))])
+			cmd_lst.append(['../'+abs_build_folder+'/peer',str(ip),str(int(self.portrange[i])),str(int(idrange[i])),str(dst_peer[0]),str(dst_peer[1])])
+			peer_lst.append([str(ip),str(int(self.portrange[i]))])
 
-		for i in range(0,peercount):
-			print(cmd_lst[i])
+		for i in range(0,self.peercount):
+			#print(cmd_lst[i])
 			stderror = "PEER_ERR_"+str(i)+".txt"
 			stdout = "PEER_OUT_"+str(i)+".txt"
 
 			x = threading.Thread(target=thread_function, args=(cmd_lst[i],str(i),Logger(stdout).logger,Logger(stderror).logger))
 			x.start()
-			time.sleep(0.1)
+			#for i in range(1,0,-1):
+			time.sleep(1)
+				#print("Starting next peer in :"+str(i))
+				#pass
 
 if __name__ == '__main__':
 	unittest.main()
